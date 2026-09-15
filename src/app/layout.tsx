449fae38e493694608f3e08fbdc70604d6ebcc38
saarthakgit <saarthakgid@gmail.com>
2026-09-15 20:01:36 +0530

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from 'next/font/local';
 import "./globals.css";
 import PromoBar from "../components/promobar";
+import Script from "next/script";
 
 const zalandoSans = localFont({
   src: [
@@ -172,6 +173,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-M333DMQ8');
+            `,
+          }}
+        />
         <style>{`
           body { 
             font-family: var(--font-zalando), sans-serif !important;
@@ -179,6 +193,14 @@ export default function RootLayout({
         `}</style>
       </head>
       <body className={`${zalandoSans.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=GTM-M333DMQ8`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <PromoBar/>
         
         {/* SAFE INJECTION: Replaces < with unicode to prevent XSS */}
